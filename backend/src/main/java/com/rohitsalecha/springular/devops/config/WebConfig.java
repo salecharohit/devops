f6e55bad1a99b5697c655097a1894d0734f6ef5e
Rohit Salecha <i@rohitsalecha.com>
2020-02-10 19:57:52 +0530

    added custom logging

---
+++ backend/src/main/java/com/rohitsalecha/springular/devops/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.rohitsalecha.springular.devops.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.rohitsalecha.springular.devops.interceptor.RequestInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer  {
+	@Autowired
+	private RequestInterceptor requestInterceptor;
+	
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(requestInterceptor);
+    }
+}
